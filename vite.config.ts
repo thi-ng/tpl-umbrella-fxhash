8f3a63f3dbebca65cf2e77b136c35c30201319bc
Karsten Schmidt <k@thi.ng>
2023-02-10 21:25:25 +0100

    minor: reformat

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-    plugins: [createHtmlPlugin({ minify: true })],
+	plugins: [createHtmlPlugin({ minify: true })],
 });
