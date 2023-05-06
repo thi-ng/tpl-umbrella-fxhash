85cda10307beb6e835478678b11c9ae808168175
Karsten Schmidt <k@thi.ng>
2023-05-06 15:29:27 +0200

    build: update vite config (sourcemapIgnoreList)

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
+	server: {
+		// https://developer.chrome.com/articles/x-google-ignore-list/
+		// https://vitejs.dev/config/server-options.html#server-sourcemapignorelist
+		sourcemapIgnoreList: (path) =>
+			path.includes("node_modules") && !path.includes("@thi.ng"),
+	},
 	plugins: [createHtmlPlugin({ minify: true })],
 });
