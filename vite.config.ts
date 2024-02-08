1c2a51064541171a01b51767afab934c41c04356
nclslbrn <nclslbrn@gmail.com>
2024-02-08 12:14:29 +0100

    Replacement of the fxhash.js file with its minified version, so that it can be detected by the sandbox and when the project is published.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-	server: {
+	// Remove hash according to https://github.com/fxhash/fxhash-boilerplate
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
+  server: {
 		// https://developer.chrome.com/articles/x-google-ignore-list/
 		// https://vitejs.dev/config/server-options.html#server-sourcemapignorelist
 		sourcemapIgnoreList: (path) =>
